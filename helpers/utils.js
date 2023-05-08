47e85bc203ec4a97adcc7f27db2041841c0f1307
jtpheonix13 <jgtihunt@comcast.net>
2023-05-07 21:42:09 -0500

    added a helper file with utils.js page for reading and writing file functions

---
+++ helpers/utils.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const util = require('util');
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+// writes data to the destination
+const writeToFile = (destination, content) => {
+    fs.writeFile(destination, JSON.stringify(contetn, null, 4), (err) => {
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    });
+};
+
+// Reads the db.json file and adds new data to the file
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
